1c7c6948e3e0d1f7dfc128b03ca9a00871f6c885
Sergio <sergiosepulveda09@live.com>
2021-06-27 17:03:50 -0700

    Initial Commit

---
+++ WaveAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaveAnimation
+//
+//  Created by Sergio Sepulveda on 2021-06-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaveAnimation/WaveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveAnimationApp.swift
+//  WaveAnimation
+//
+//  Created by Sergio Sepulveda on 2021-06-27.
+//
+
+import SwiftUI
+
+@main
+struct WaveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
